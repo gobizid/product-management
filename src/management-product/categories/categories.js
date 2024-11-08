27f9b84cd48a28c2b51593c4e8c52f3a190dc697
KostsPlayer <azka.nuril070@gmail.com>
2024-11-08 19:42:34 +0700

    fix some issues pt.7

--- src/management-product/categories/categories.js
+++ src/management-product/categories/categories.js
@@ -1,15 +1,15 @@
-import loadComponent from "src/helpers/loadComponent.js";
-import sidebarTogle from "src/components/sidebar/sidebar.js";
-import smoothScroll from "src/helpers/smoothScroll.js";
-import getAllCateogories from "src/management-product/categories/fetch.js";
+import loadComponent from "/src/helpers/loadComponent.js";
+import sidebarTogle from "/src/components/sidebar/sidebar.js";
+import smoothScroll from "/src/helpers/smoothScroll.js";
+import getAllCateogories from "/src/management-product/categories/fetch.js";
 
 export default function loadManagementProductCategories() {
   const promises = [
-    loadComponent("aside.sidebar", "src/components/sidebar/sidebar.html"),
-    loadComponent("header.topbar", "src/components/topbar/topbar.html"),
+    loadComponent("aside.sidebar", "/src/components/sidebar/sidebar.html"),
+    loadComponent("header.topbar", "/src/components/topbar/topbar.html"),
     loadComponent(
       "",
-      "src/management-product/categories/categories.html"
+      "/src/management-product/categories/categories.html"
     ),
   ];
 
